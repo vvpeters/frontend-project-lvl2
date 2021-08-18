0ba95ffee520570dcad1188e223c5097d91fc2bf
Valentina Peters <pvv1827@gmx.de>
2021-08-18 22:52:01 +0200

    add nodejs.yml

--- Makefile
+++ Makefile
@@ -6,3 +6,5 @@ publish:
 	npm link
 lint:
 	npx eslint .
+test:
+	npm test
---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@jest/globals';
+import genDiff from '../src/index.js';
+
+test('diff', () => {
+  const pathToFile1 = '../src/file1.json';
+  const pathToFile2 = '../src/file2.json';
+  // const str = 'hello';
+  expect(genDiff(pathToFile1, pathToFile2)).toEqual(`{
+    - follow: false
+      host: hexlet.io
+    - proxy: 123.234.53.22
+    - timeout: 50
+    + timeout: 20
+    + verbose: true
+  }`);
+});
