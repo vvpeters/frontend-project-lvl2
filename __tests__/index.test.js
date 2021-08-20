f7196f1a3ec5afe95f48b57309b5d52b82a177a4
Valentina Peters <pvv1827@gmx.de>
2021-08-20 22:11:58 +0200

    Fixed tests and added .npmrc

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,11 +6,11 @@ test('diff', () => {
   const pathToFile2 = './src/file2.json';
   // const str = 'hello';
   expect(genDiff(pathToFile1, pathToFile2)).toEqual(`{
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
-  }`);
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`);
 });
