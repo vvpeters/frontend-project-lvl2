6290cf3b20ba72c3d7e8f73fb32bcbfa8c7e8f58
Valentina Peters <pvv1827@gmx.de>
2021-08-24 14:02:18 +0200

    fixed tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,10 +2,11 @@ import { test, expect } from '@jest/globals';
 import { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
 
+const result = readFileSync('__tests__/__fixtures__/result_index.txt', 'utf-8');
+
 test('diffJsonJson', () => {
   const pathToFile1 = '__tests__/__fixtures__/file1.json';
   const pathToFile2 = '__tests__/__fixtures__/file2.json';
-  const result = readFileSync('__tests__/__fixtures__/result_index.txt', 'utf-8');
 
   expect(genDiff(pathToFile1, pathToFile2)).toBe(result);
 });
@@ -13,7 +14,6 @@ test('diffJsonJson', () => {
 test('diffYamlYaml', () => {
   const pathToFile1 = '__tests__/__fixtures__/file1.yaml';
   const pathToFile2 = '__tests__/__fixtures__/file2.yaml';
-  const result = readFileSync('__tests__/__fixtures__/result_index.txt', 'utf-8');
 
   expect(genDiff(pathToFile1, pathToFile2)).toBe(result);
 });
@@ -21,7 +21,6 @@ test('diffYamlYaml', () => {
 test('diffYamlJson', () => {
   const pathToFile1 = '__tests__/__fixtures__/file1.yaml';
   const pathToFile2 = '__tests__/__fixtures__/file2.json';
-  const result = readFileSync('__tests__/__fixtures__/result_index.txt', 'utf-8');
 
   expect(genDiff(pathToFile1, pathToFile2)).toBe(result);
 });
